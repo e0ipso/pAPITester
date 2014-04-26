55f54bb43ba22cedba1600417c1789e16b8d16f2
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-04-26 11:31:17 +0200

    Ignore *.pyc files

--- pAPItester/client.py
+++ pAPItester/client.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import argparse
 import json
 from urllib2 import Request
--- testapi.py
+++ testapi.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python
-
 import pAPItester.client
 import json
 
 def main():
   args = pAPItester.client.getArguments()
   try:
-    json_data = open(args.config_file).read()
-    parsed_object = json.loads(json_data)
+    json_string = open(args.config_file).read()
+    parsed_object = json.loads(json_string)
     print parsed_object
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
