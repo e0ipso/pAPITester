fa78efa190d8c61f35ddc77dc7024203f43c7998
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2014-04-26 10:39:54 +0200

    WIP

--- testapi.py
+++ testapi.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
 import src.client
+import json
+import re
 
 def main():
   args = src.client.getArguments()
-  print args
+  try:
+    json_data = open(args.config_file).read()
+    print json.loads(json_data)
+  except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
 if __name__ == '__main__':
   main()
